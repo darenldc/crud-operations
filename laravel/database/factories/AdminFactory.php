70545142063a235feff051765ffb319b542b46ee
Daren Ace Dela Cruz <daren@bluehive.com.ph>
2024-05-18 13:00:02 +0800

    admin mfsc and login logout function for admin

---
+++ laravel/database/factories/AdminFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Admin;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Admin>
+ */
+class AdminFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Admin::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'username' => 'admin',
+            'password' => Hash::make('password'),
+        ];
+    }
+}
